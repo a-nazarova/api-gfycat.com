932878b141d4f5cd1c7bd1f06dd412d90f9718b4
a_nazarova <angelina@rerotor.ru>
2023-05-30 13:36:06 +0300

    add tests for patch "Me"

--- gfycat_auth.py
+++ gfycat_auth.py
@@ -10,8 +10,8 @@ def get_token_gfycat(credentials):
 
 credentials = {
     "grant_type": "password",
-    "password": "password",
-    "username": "username"
+    "password": "Mus64103",
+    "username": "a_nazarova"
 }
 
 token_gfycat = get_token_gfycat(credentials)
