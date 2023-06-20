689b5030e1d731c8a571915d450094209364f260
a_nazarova <angelina@rerotor.ru>
2023-06-20 21:07:37 +0300

    fix

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import constant
 
 
 def get_token_gfycat():
+    """Функция возвращает авторизационный токен по логину и паролю"""
     token = requests.post("https://weblogin.gfycat.com/oauth/weblogin", json=constant.CREDENTIALS)
     token_body = token.json()
     token = token_body["access_token"]
